f69fd15cd2ebf47af1ca4b1df680a55a55bf30e3
priyaselvakumar97 <priyaselvakumar276@gmail.com>
2024-03-12 10:25:37 +0530

    busReservationSystem

---
+++ src/busReservation/Booking.java
@@ -0,0 +1,15 @@
+package busReservation;
+import java.util.*;
+public class Booking {
+
+	public static void main(String[] args) {
+		int busNo;
+		String PassengerName;
+		Date date;
+		
+		
+		
+
+	}
+
+}
---
+++ src/busReservation/Bus.java
@@ -0,0 +1,12 @@
+package busReservation;
+
+public class Bus {
+
+	public static void main(String[] args) {
+		int busNo;
+		boolean AC;
+		int capacity;
+
+	}
+
+}
